640b329d9ca9aecf2b3a839ecbe488c50f1679e4
TinaTech-Developers <tinashephiri0@gmail.com>
2026-01-05 13:44:57 +0200

    finishedd

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Footer from "./components/footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  fallback: ["sans-serif"], // fallback if Google Fonts fails
 });
 
 const geistMono = Geist_Mono({
